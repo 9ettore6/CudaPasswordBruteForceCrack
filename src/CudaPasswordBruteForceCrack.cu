74e0b5f68a8621e1dce16e588695d9d60ba69c4c
luca <lucaciabo@hotmail.it>
2018-11-28 10:32:17 +0100

    Fixed hashes allocation e copy from host to device

--- src/CudaPasswordBruteForceCrack.cu
+++ src/CudaPasswordBruteForceCrack.cu
@@ -1,7 +1,7 @@
 /*
  ============================================================================
  Name        : CudaPasswordBruteForceCrack.cu
- Author      : Ettore
+ Author      : Culo
  Version     :
  Copyright   : Your copyright notice
  Description : CUDA compute reciprocals
@@ -13,6 +13,9 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
+#include <thrust/host_vector.h>
+#include <thrust/device_vector.h>
+#include <thrust/sort.h>
 
 
 #include "crypt.h"
@@ -34,7 +37,7 @@ static void CheckCudaErrorAux(const char *file, unsigned line,
   exit(1);
 }
 __constant__ char dictionary[10]={'0','1','2','3','4','5','6','7','8','9'};
-__global__ void kernel(char** results, char** hashes, int dim) {
+__global__ void kernel(char** hashes, int dim, char** _a) {
 	int mI = threadIdx.y+blockIdx.y*blockDim.y;
 	int yI = threadIdx.x+blockIdx.x*blockDim.x + 1940;
 	int dI = threadIdx.z;
@@ -84,7 +87,7 @@ __global__ void kernel(char** results, char** hashes, int dim) {
 
 
 	char yyyymmdd[9] = {yyyy[0],yyyy[1],yyyy[2],yyyy[3],mm[0],mm[1],dd[0],dd[1],0};
-	printf("%s \n", yyyymmdd);
+	printf("%s \n",_a[70]);
 	for(int i=0; i<dim; i++){
    /* char* pwd="";
     char* psw;
@@ -100,58 +103,67 @@ __global__ void kernel(char** results, char** hashes, int dim) {
 
 int main(void)
 {
-  #define dim 100
-  char * resultsHost[dim];
-  char ** hashes;
-  char ** results;
-  FILE * fp;
-  char * line = NULL;
-  size_t len = 0;
-  ssize_t read;
-  char* hashesHost[dim];
-  int k=0;
-  fp = fopen("PswDb/db100.txt", "r");
-  while ((read = getline(&line, &len, fp)) != -1) {
-    char* hash =(char*) malloc(sizeof(char)*13);
-    for(int i = 0; i<13; i++){
-      hash[i]=line[i+9];
-    }
-    hashesHost[k]=hash;
-    k++;
-  }
-  fclose(fp);
-  free(line);
-  char* psw;
-  char* salt = "parallel";
-  //psw = crypt("19961024", salt);
-  std::cout<< "19961024 " << psw << "\n";
-  printf("--- %s\n", hashesHost[0]);
-
-  // allocate device memory
+	#define dim 100
+	char * resultsHost[dim];
+	char ** results;
+	FILE * fp;
+	char * line = NULL;
+	size_t len = 0;
+	ssize_t read;
+	char* hashesHost[dim];
+	int k=0;
+	fp = fopen("PswDb/db100.txt", "r");
+	while ((read = getline(&line, &len, fp)) != -1) {
+	char* hash =(char*) malloc(sizeof(char)*14);
+	for(int i = 0; i<13; i++){
+	  hash[i]=line[i+9];
+	}
+	hash[13]=0;
+	hashesHost[k]=hash;
+	k++;
+	}
+	fclose(fp);
+	free(line);
+	char* psw;
+	char* salt = "parallel";
+	//psw = crypt("19961024", salt);
+	printf("--- %s\n", hashesHost[0]);
 
 
-  CUDA_CHECK_RETURN(
-		  cudaMalloc((void **) &hashes, sizeof(char) * 13 * dim));
+	char * _s[dim];
+	char ** _a;
 
-  CUDA_CHECK_RETURN(
+	for (int i = 0; i < 100; i++) {
+
+		CUDA_CHECK_RETURN( cudaMalloc((void **)&_s[i], 13 * sizeof(char)) );
+
+		CUDA_CHECK_RETURN( cudaMemcpy(_s[i], hashesHost[i], 13 * sizeof(char), cudaMemcpyHostToDevice) );
+
+	  }
+	CUDA_CHECK_RETURN( cudaMalloc((void ***)&_a, dim * sizeof(char*)) );
+
+	CUDA_CHECK_RETURN( cudaMemcpy(_a, _s, dim * sizeof(char*), cudaMemcpyHostToDevice) );
+
+
+
+
+	CUDA_CHECK_RETURN(
 		  cudaMalloc((void **) &results, sizeof(char) * 13 * dim));
 
-  // copy from host to device memory
-  CUDA_CHECK_RETURN(
-      cudaMemcpy(hashes, hashesHost, dim * 13 * sizeof(char),
-          cudaMemcpyHostToDevice));
 
 
-  //@@ INSERT CODE HERE
-  dim3 dimGrid(7,4);
-  dim3 dimBlock(10,3,31);
-  kernel<<<dimGrid, dimBlock>>>(results,hashes,dim);
-  // copy results from device memory to host
 
-  CUDA_CHECK_RETURN(
-      cudaMemcpy(resultsHost, results, dim * 13 * sizeof(char),
-          cudaMemcpyDeviceToHost));
-  cudaFree(hashes);
-  cudaFree(results);
-  return 0;
+	//@@ INSERT CODE HERE
+	dim3 dimGrid(7,4);
+	dim3 dimBlock(10,3,31);
+	kernel<<<dimGrid, dimBlock>>>(results,dim,_a);
+	printf("culo");
+	// copy results from device memory to host
+
+	CUDA_CHECK_RETURN(
+	  cudaMemcpy(resultsHost, results, dim * 13 * sizeof(char),
+		  cudaMemcpyDeviceToHost));
+
+	cudaFree(results);
+	return 0;
 }
