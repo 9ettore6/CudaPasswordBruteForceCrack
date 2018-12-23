8ef72b952cbeff48765d624860617460db1828de
ettore <ettorecelozzi@gmail.com>
2018-12-23 15:44:09 +0100

    bug fix

--- src/CudaPasswordBruteForceCrack.cu
+++ src/CudaPasswordBruteForceCrack.cu
@@ -13,10 +13,6 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <fstream>
-#include <thrust/host_vector.h>
-#include <thrust/device_vector.h>
-#include <thrust/sort.h>
-#include "crypt.h"
 #include "c_utils.h"
 #include "des.h"
 #include "des_utils.h"
@@ -42,31 +38,25 @@ static void CheckCudaErrorAux(const char *file, unsigned line,
       << err << ") at " << file << ":" << line << std::endl;
   exit(1);
 }
-//__constant__ char dictionary[10]={'0','1','2','3','4','5','6','7','8','9'};
-__global__ void kernel(int* resultsDevice, int dim, uint64_t* hashesDevice, uint64_t* testTh) {
+
+__global__ void kernel(int* resultsDevice, int dim, uint64_t* hashesDevice) {
 	int mI = threadIdx.y+blockIdx.y*blockDim.y;
 	int yI = threadIdx.x+blockIdx.x*blockDim.x + 1940;
 	int dI = threadIdx.z;
-	//resultsDevice[(yI-1940)*12*31+mI*31+dI] = 1;
 	uint64_t key = yI*10000+mI*100+dI;
 	uint64_t encoded = 0;
 	encoded = full_des_encode_block(key, key);
 	for(int i=0;i<dim;i++){
-		//printf("%d -- %d\n", hashesDevice[i], encoded);
 		if (hashesDevice[i] == encoded){
 			resultsDevice[i] = 1;
-		}else{
-			testTh[(yI-1940)*12*31+mI*31+dI] = key;
 		}
 	}
 }
 
 
-int main(void)
-{
+int main(void){
 	#define dim 100
 	int resultsHost[dim];
-	uint64_t hostTestTH[26040];
 	FILE * fp;
 	char * line = NULL;
 	size_t len = 0;
@@ -89,7 +79,6 @@ int main(void)
 	//GPU memory allocation
 	uint64_t* hashesDevice;
 	int* resultsDevice;
-	uint64_t* testTh;
 
 	CUDA_CHECK_RETURN( cudaMalloc((void **)&hashesDevice, dim * sizeof(uint64_t)) );
 
@@ -97,38 +86,28 @@ int main(void)
 
 	CUDA_CHECK_RETURN( cudaMalloc((void **) &resultsDevice, sizeof(int) * dim));
 
-	CUDA_CHECK_RETURN( cudaMalloc((void **) &testTh, sizeof(uint64_t) * 26040));
-	//@@ INSERT CODE HERE
-	dim3 dimGrid(7,4);
-	dim3 dimBlock(10,3,31);//
-	kernel<<<dimGrid,dimBlock>>>(resultsDevice,dim,hashesDevice,testTh);
+	dim3 dimGrid(8,5);
+	dim3 dimBlock(10,3,32);//
+	clock_t start = clock();
+	kernel<<<dimGrid,dimBlock>>>(resultsDevice,dim,hashesDevice);
 	// copy results from device memory to host
-	/*for(int i = 0; i < dim; i++){
-		resultsHost[i] = 0;
-	}*/
-	//printf("***********%d\n",resultsHost[5]);
+	clock_t end = clock();
+	cudaDeviceSynchronize();
+	float seconds = (float) (end - start) / CLOCKS_PER_SEC;
 	CUDA_CHECK_RETURN(
 	  cudaMemcpy(resultsHost, resultsDevice, dim * sizeof(int),
 		  cudaMemcpyDeviceToHost));
-	CUDA_CHECK_RETURN(
-		  cudaMemcpy(hostTestTH, testTh, dim * sizeof(uint64_t),
-			  cudaMemcpyDeviceToHost));
-	//printf("***********%d\n",resultsHost[15]);
+
 	cudaFree(hashesDevice);
 	cudaFree(resultsDevice);
 
 	int count = 0;
-	int countff = 0;
 	for(int i = 0; i < dim; i++){
 		if(resultsHost[i] == 1){
 			count++;
-			printf("hash ok: %d\n", resultsHost[i]);
-		}else{
-			countff++;
-			printf("hash nope: %d\n", hostTestTH[i]);
 		}
 	}
-	printf("ccc: %d\n", count);
-	printf("fff: %d", countff);
+	printf("found hashes: %d\n", count);
+	printf("time: %f",seconds);
 	return 0;
 }
